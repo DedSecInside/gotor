6e1d007aedf4fa2d590bc71b34f278ab9b61020a
KingAkeem <akeemtlking@gmail.com>
2018-06-03 16:17:47 -0400

    Seting up webpack and react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+class TorBotForm extends React.Component {
+    render() {
+        return ( <
+            form >
+            <
+            input type = "text" > < /input>  < /
+            form >
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }, {
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                    importLoaders: 1,
+                    localIdentName: "[name]_[local]_[hash:base64]",
+                    minimize: true
+                }
+            }]
+        }]
+    },
+    plugins: [htmlPlugin]
+};
