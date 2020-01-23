53c2598ec2fd778320cd5c27b620314d9ac50fd1
Abdallah Kawji <abdallah@styla.com>
2020-01-23 20:26:57 +0100

    refactoring: using react hooks + functional components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,85 +1,70 @@
-import css from './index.css';
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import DisplayURLs from "./options.jsx";
+import './index.css';
 
-class TorBotArguments extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <select className="dropdown-select" onChange={this.props.handler}>
+const TorBotArguments = (props) => (
+    <select className="dropdown-select" onChange={props.handler}>
         {
-        this.props.args.map((arg) => {
-            return <option name='argument' key={arg} value={arg}>{arg}</option>;
-          })
+            props.args.map((arg) =>
+                <option name='argument' key={arg} value={arg}>{arg}</option>
+            )
         }
-      </select>
-    )
-  }
-}
+    </select>
+);
+
+export const TorBotForm = (props) => {
+    const [option, setOption] = useState('Retrieve Mail');
+    const [website, setWebsite] = useState('');
+
+    const inputChange = (event) =>
+        setWebsite(event.target.value);
+
+    const handleChange = (event) =>
+        setOption(event.target.value);
 
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        if (option === 'Retrieve URLs') {
+            fetch('http://localhost:8008/LIVE', {
+                body: JSON.stringify({
+                    option,
+                    website,
+                }),
+                method: 'POST'
+            }).then(response => {
+                // object has 'websites' property that contains an array of links
+                return response.json();
+            }).then(data => {
+                handleURLs(data);
+            }).catch(error => {
+                alert(error);
+            });
+        }
+    };
 
-class TorBotForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {option: 'Retrieve Mail', website: ''};
-    this.handleChange = this.handleChange.bind(this);
-    this.inputChange = this.inputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  inputChange(event) {
-    this.setState({option: this.state.option, website: event.target.value});
-  }
-  handleChange(event) {
-    this.setState({option: event.target.value, website: this.state.website});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    if (this.state.option === 'Retrieve URLs') {
-      fetch('http://localhost:8008/LIVE', {
-        body: JSON.stringify(this.state),
-        method: 'POST'
-      }).then(response => {
-        // object has 'websites' property that contains an array of links
-        return response.json();
-      }).then(data => {
-        handleURLs(data);
-      }).catch(error => {
-        alert(error);
-      });
-      }
-    }
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit} id ="mainForm">
-              <label id='siteFieldTitle'> Website:
-                <input onChange={this.inputChange} id='siteName' type='text' name='website'/>
-              </label>
-              <br/>
-              <label id='optionTitle'> Option:
-                <TorBotArguments handler={this.handleChange}args={this.props.args}/>
+    return (
+        <form onSubmit={handleSubmit} id="mainForm">
+            <label id='siteFieldTitle'> Website:
+                <input onChange={inputChange} id='siteName' type='text' name='website'/>
+            </label>
+            <br/>
+            <label id='optionTitle'> Option:
+                <TorBotArguments handler={handleChange} args={props.args}/>
             </label>
             <br/>
             <input id='submitBtn' type="submit"/>
-           </form>
-        );
-    }
-}
+        </form>
+    );
+};
 
-export default TorBotForm;
 
 function handleURLs(data) {
-  ReactDOM.render(<DisplayURLs flags={flags}websites={data.websites}/>, document.getElementById('root'));
+    ReactDOM.render(<DisplayURLs flags={flags} websites={data.websites}/>, document.getElementById('root'));
 }
 
-var flags = ['Retrieve Emails', 'Retrieve URLs', 'Retrieve Information']
+const flags = ['Retrieve Emails', 'Retrieve URLs', 'Retrieve Information'];
 ReactDOM.render(<TorBotForm args={flags}/>, document.getElementById('root'));
 
--- client/src/options.jsx
+++ client/src/options.jsx
@@ -1,49 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import TorBotForm from "./index.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {TorBotForm} from './index';
 
-class DisplayURLs extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const DisplayURLs = (props) => {
 
-  goBack() {
-    ReactDOM.render(<TorBotForm args={this.props.flags}/>, document.getElementById('root'))
-  }
+    const goBack = () => {
+        ReactDOM.render(<TorBotForm args={this.props.flags}/>, document.getElementById('root'))
+    };
 
-  render() {
     return (
-      <table>
-        <thead>
-          <tr>
-            <th> URLS FOUND </th>
-          </tr>
-        </thead>
-        <tfoot>
-          <tr>
-            <td>
-              <button onClick={this.goBack} id="backButton">BACK</button>
-            </td>
-          </tr>
-        </tfoot>
-        <tbody>
-        {
-          Object.keys(this.props.websites).map((website, idx) => {
-            if (this.props.websites[website] == true) {
-              return <tr name="website" key={website}>
-                      <td id="goodLink">{idx+1}. {website}</td>
-                    </tr>;
-            } else {
-              return <tr name="website" key={website}>
-                      <td id="badLink">{idx+1}. {website}</td>
-                    </tr>;
+        <table>
+            <thead>
+            <tr>
+                <th> URLS FOUND</th>
+            </tr>
+            </thead>
+            <tfoot>
+            <tr>
+                <td>
+                    <button onClick={goBack} id="backButton">BACK</button>
+                </td>
+            </tr>
+            </tfoot>
+            <tbody>
+            {
+                Object.keys(props.websites).map((website, idx) =>
+                    <tr key={website}>
+                        <td className={props.websites[website] ? 'goodLink' : 'badLink'}>{idx + 1}. {website}</td>
+                    </tr>
+                )
             }
-          })
-        }
-      </tbody>
-    </table>
+            </tbody>
+        </table>
     )
-  }
-}
+};
 
 export default DisplayURLs;
