7e893d0289370b86bca6445cfc351dfe46b566bf
Akeem King <akeemtlking@gmail.com>
2021-09-13 02:33:48 -0400

    Add more unit tests and assert package

--- api/handlers_test.go
+++ api/handlers_test.go
@@ -1,22 +1,29 @@
 package api
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/jarcoal/httpmock"
 	"net/http"
+
+	"github.com/jarcoal/httpmock"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetTorIP(t *testing.T) {
-	myMockPage := `<!DOCTYPE html>
+func newPage(title string, body string) string {
+	baseHTML := `<!DOCTYPE html>
 		<html lang=en-US class=no-js>
-			<head><title>Tor Project</title></head>
-			<body><strong>Random IP Address</strong></body>
-		</html>`
+			<head><title>%s</title></head>
+			<body>%s</body>
+	</html>`
+	return fmt.Sprintf(baseHTML, title, body)
+}
 
+func TestGetTorIP(t *testing.T) {
 	httpmock.Activate()
+	page := newPage("Tor Project", "<strong>Random IP Address</strong>")
 	httpmock.RegisterResponder("GET", "https://check.torproject.org/",
-		httpmock.NewStringResponder(200, myMockPage))
+		httpmock.NewStringResponder(200, page))
 
 	ip, err := getTorIP(http.DefaultClient)
 	if err != nil {
@@ -25,26 +32,40 @@ func TestGetTorIP(t *testing.T) {
 	httpmock.DeactivateAndReset()
 
 	if ip != "Random IP Address" {
-		t.Error("The IP address was not succesfully extracted.")
+		t.Error("The IP address was not successfully extracted.")
 	}
 }
 
 func TestGetEmails(t *testing.T) {
-	myMockPage := `<!DOCTYPE html>
-		<html lang=en-US class=no-js>
-			<head><title>Tor Project</title></head>
-			<body><a href="mailto:random@gmail.com">Email me</a></body>
-		</html>`
-
 	link := "https://random.com"
 	httpmock.Activate()
+	page := newPage("Random Site", `<a href="mailto:random@gmail.com">Email me</a>`)
 	httpmock.RegisterResponder("GET", link,
-		httpmock.NewStringResponder(200, myMockPage))
+		httpmock.NewStringResponder(200, page))
 
 	emails := getEmails(http.DefaultClient, link)
 	httpmock.DeactivateAndReset()
 
 	if len(emails) != 1 || emails[0] != "random@gmail.com" {
-		t.Error("The email address was not succesfully extracted.")
+		t.Error("The email address was not successfully extracted.")
+	}
+
+	httpmock.Activate()
+	page = newPage("Random Site", `<a href="mailto:random@gmail.com">email me</a>
+					<a href="mailto:random@yahoo.com">email me</a>
+					<a href="mailto:random@protonmail.com">email me</a>
+					<a href="mailto:random@outlook.com">email me</a>`)
+	httpmock.RegisterResponder("GET", link,
+		httpmock.NewStringResponder(200, page))
+
+	emails = getEmails(http.DefaultClient, link)
+	httpmock.DeactivateAndReset()
+
+	assert.Contains(t, emails, "random@gmail.com", "Gmail address not parsed")
+	assert.Contains(t, emails, "random@yahoo.com", "Yahoo address not parsed")
+	assert.Contains(t, emails, "random@protonmail.com", "Protonmail address not parsed")
+	assert.Contains(t, emails, "random@outlook.com", "Outlook address not parsed")
+	if len(emails) != 4 {
+		t.Error("The email address was not successfully extracted.")
 	}
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
+	github.com/stretchr/testify v1.7.0
 	github.com/xuri/excelize/v2 v2.4.1
 	golang.org/x/net v0.0.0-20210825183410-e898025ed96a
 	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
