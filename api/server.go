ea3cd1652c69bad06941dffe496bc17838bb56b2
Akeem King <akeemtlking@gmail.com>
2023-10-31 09:06:23 -0400

    Add HTTP method to server logs

--- api/server.go
+++ api/server.go
@@ -26,7 +26,7 @@ func New(client *http.Client, host string, port int) *Server {
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
-		log.Println(r.RequestURI)
+		log.Printf("%s %s\n", r.Method, r.RequestURI)
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
 	})
